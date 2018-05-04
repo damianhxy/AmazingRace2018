5dc5c89d4686f92b28a973909310bdc40d4e4980
Damian Ho <damian_ho_xu_yang@yahoo.com>
2018-05-04 09:43:04 +0800

    Use btn-grp and add confirmation Add prizes

--- public/js/main.js
+++ public/js/main.js
@@ -4,4 +4,10 @@ $(function() {
     // Active Link
     var active = $("a.nav-link[href='" + location.pathname + "']");
     active.addClass("active");
+
+    $("form.adminForm").submit(function(e) {
+        e.preventDefault();
+        if (confirm("Are you sure?"))
+            e.target.submit();
+    });
 });
